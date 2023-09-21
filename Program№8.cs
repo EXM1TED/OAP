180bd39c5cc0f54655a515c02a93dd6b2c6edede
EXM1TED <115313603+EXM1TED@users.noreply.github.com>
2023-09-21 15:14:38 +0300

    ДЗ 22.09.2023

---
+++ Program№8.cs
@@ -0,0 +1,29 @@
+﻿// стр. 20 №8
+//Определить количество положительных чисел в заданных числах a, b, c
+Console.WriteLine("Введите число a");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число b");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число c");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int k = 0;
+
+if (a > 0)
+{
+    k = k + 1;
+}
+
+if (b > 0)
+{
+    k = k + 1;
+}
+
+if (c > 0)
+{
+    k = k + 1;
+}
+
+Console.WriteLine($"Колличество положительных чисел {k}");
