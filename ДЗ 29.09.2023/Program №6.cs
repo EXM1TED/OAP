e905ed48c7bf456beda730e89e2ee508564d6f89
EXM1TED <115313603+EXM1TED@users.noreply.github.com>
2023-09-27 18:12:51 +0300

    Add files via upload

---
+++ ДЗ 29.09.2023/Program №6.cs
@@ -0,0 +1,29 @@
+﻿// стр.28 №6
+// Получить словесное описание школьных отметок
+Console.WriteLine("Ввдеите оценку: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+while (a < 1 | a > 5)
+{
+    Console.WriteLine("Ошибка, введите число ещё раз");
+    a = Convert.ToInt32(Console.ReadLine());
+}
+
+switch (a)
+{
+    case 1:
+        Console.WriteLine("Плохо");
+        break;
+    case 2:
+        Console.WriteLine("Неудовлетворительно");
+        break;
+    case 3:
+        Console.WriteLine("Удовлетворительно");
+        break;
+    case 4:
+        Console.WriteLine("Хорошо");
+        break;
+    case 5:
+        Console.WriteLine("Отлично");
+        break;
+}
