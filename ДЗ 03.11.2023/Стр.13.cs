28fb8453927a19312c19abbb8c9064607036ee3a
EXM1TED <115313603+EXM1TED@users.noreply.github.com>
2023-11-02 19:29:46 +0300

    Add files via upload

---
+++ ДЗ 03.11.2023/Стр.13.cs
@@ -0,0 +1,27 @@
+﻿// №2
+//Console.WriteLine("Введите координату по оси x");
+//double x = Convert.ToDouble(Console.ReadLine());
+
+//Console.WriteLine("Введите координату по оси x");
+//double y = Convert.ToDouble(Console.ReadLine());
+
+
+//Console.WriteLine(x * x + y * y == 9 || x * x + y * y == 25 && y <= 5 && x <= 5 && x >= 0);
+
+//№3
+//Console.WriteLine("Введите координату по оси x");
+//double x = Convert.ToDouble(Console.ReadLine());
+
+//Console.WriteLine("Введите координату по оси x");
+//double y = Convert.ToDouble(Console.ReadLine());
+
+//Console.WriteLine((x >= -2 && x <= 2) || (x >= -4 && x <= 5) && y >= -3 && y <= 4 );
+
+//№4
+//Console.WriteLine("Введите координату по оси x");
+//double x = Convert.ToDouble(Console.ReadLine());
+
+//Console.WriteLine("Введите координату по оси x");
+//double y = Convert.ToDouble(Console.ReadLine());
+
+//Console.WriteLine(y => x - 5 || y => x - 3 || y <= x + 3 || y <= -x + 11);
