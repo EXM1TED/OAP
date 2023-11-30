28a43ca35dc6f3f909fe3d0b00c5cc9a90306c82
EXM1TED <115313603+EXM1TED@users.noreply.github.com>
2023-11-30 21:35:19 +0300

    Update Задание 14.py

--- ДЗ 01 12 2023/Задание 14.py
+++ ДЗ 01 12 2023/Задание 14.py
@@ -8,7 +8,7 @@ def lcm(x, y):
 
 def add_fractions(a, b, c, d):
     common_denominator = lcm(b, d)
-    numerator_sum = a * (common_denominator // b) + c * (common_denominator // d)
+    numerator_sum = a * (common_denominator // b) - c * (common_denominator // d)
     common_divisor = gcd(numerator_sum, common_denominator)
     result_numerator = numerator_sum // common_divisor
     result_denominator = common_denominator // common_divisor
